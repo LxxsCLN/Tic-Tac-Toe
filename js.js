b2e522b0c4343b80ca186b9f9b934e6c22c8e1d7
LxxsCLN <113736900+LxxsCLN@users.noreply.github.com>
2023-03-31 21:26:12 -0600

    add tie function to game. add user input for names

--- js.js
+++ js.js
@@ -1,42 +1,32 @@
 const game = document.querySelector(".gameboard");
 const squares = Array.from(document.querySelectorAll(".square"));
-
 const text = document.querySelector(".text");
 const text2 = document.querySelector(".text2");
 const restart = document.querySelector(".restart");
-
-
-
-
+const submit = document.querySelector(".submit");
+const playeroneinput = document.querySelector(".playerone");
+const playertwoinput = document.querySelector(".playertwo");
 
 const gameboard = (() => {
-    let gameboardarray = ["","","","","","","","",""];
-    const updateScreen = () => {
-        for (let i in squares){
-            squares[i].innerText = gameboardarray[i];
-        }  
-        whoWon();
-        changePlayer();
-    };
+  let gameboardarray = ["", "", "", "", "", "", "", "", ""];
 
-    const click = (() => squares.forEach((square) => {
-        square.addEventListener("click", () => {
-  
-        if (square.innerText !== "") return;
-  
-        gameboardarray[square.dataset.value] = currentPlayer.symbol;
-            
-        
+  const updateScreen = () => {
+    for (const i in squares) {
+      squares[i].innerText = gameboardarray[i];
+    }
+    whoWon();
+    changePlayer();
+  };
 
-        updateScreen();
+  const click = (() => squares.forEach((square) => {
+    square.addEventListener("click", () => {
+      if (square.innerText !== "") return;
+      gameboardarray[square.dataset.value] = currentPlayer.symbol;
+      updateScreen();
+    });
+  }))();
 
-        
-  
-        
-      })
-    }))();
-
-    const checkWinner = [
+  const checkWinner = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -44,64 +34,79 @@ const gameboard = (() => {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
-    ]
+    [2, 4, 6],
+  ];
 
-    const whoWon = () => {
-        
-        for (let a = 0; a < 8; a++){
-            let x = "";
-            
-            for (let b = 0; b < 3; b++){
-                x += gameboardarray[checkWinner[a][b]]    
-            }
-            if (x === "XXX" || x === "OOO"){
-                text2.innerText = `${currentPlayer.name} WINS`;
-                stopGame()
-            }            
-        }
-        
+  const whoWon = () => {
+    let y = 0;
+    for (let a = 0; a < 8; a++) {
+      let x = "";
+
+      for (let b = 0; b < 3; b++) {
+        x += gameboardarray[checkWinner[a][b]];
+      }
+      if (x === "XXX" || x === "OOO") {
+        text2.innerText = `${currentPlayer.name} WINS`;
+        stopGame();
+      }
     }
-
-    const stopGame = () => {
-        game.classList.add("nonclick")
+    for (let x = 0; x < 9; x++) {
+      if (squares[x].innerText !== "") {
+        y++;
+      }
     }
-
-    const changePlayer = () => {
-        if (currentPlayer === playerOne){
-            currentPlayer = playerTwo;
-          }
-          else if (currentPlayer === playerTwo){
-            currentPlayer = playerOne;
-          }
-          text.innerText = `It is ${currentPlayer.name}'s turn`
-        if (game.classList.contains("nonclick")){
-            text.innerText = "";
-        }
+    console.log(y);
+    if (y === 9) {
+      text2.innerText = "It's a TIE";
+      stopGame();
     }
+  };
 
-    const restartGame = (() => {
-        restart.addEventListener("click", () => {
-            gameboardarray = ["","","","","","","","",""];
-            updateScreen();
-            currentPlayer = playerOne;
-            game.classList.remove("nonclick")
-            text2.innerText ="";
-            text.innerText = `It is ${currentPlayer.name}'s turn`
-        })      
-        })();
+  const stopGame = () => {
+    game.classList.add("nonclick");
+  };
 
+  const changePlayer = () => {
+    if (currentPlayer === playerOne) {
+      currentPlayer = playerTwo;
+    } else if (currentPlayer === playerTwo) {
+      currentPlayer = playerOne;
+    }
+    text.innerText = `It is ${currentPlayer.name}'s turn`;
+    if (game.classList.contains("nonclick")) {
+      text.innerText = "";
+    }
+  };
+
+  const restartGame = (() => {
+    restart.addEventListener("click", () => {
+      gameboardarray = ["", "", "", "", "", "", "", "", ""];
+      updateScreen();
+      currentPlayer = playerOne;
+      game.classList.remove("nonclick");
+      text2.innerText = "";
+      text.innerText = `It is ${currentPlayer.name}'s turn`;
+    });
+  })();
 })();
 
 
+
 const Player = (name, symbol) => {
-    return {name, symbol}
-  };
+    return { name, symbol }
+}
 
+let playerOne = Player("Player One", "X");
+let playerTwo = Player("Player Two", "O");
 
-
-const playerOne = Player("Player One", "X");
-const playerTwo = Player("Player Two", "O");
 let currentPlayer = playerOne;
 text.innerText = `It is ${currentPlayer.name}'s turn`;
 
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    playerOne.name = playeroneinput.value;
+    playerTwo.name = playertwoinput.value;
+    playeroneinput.value =""
+    playertwoinput.value =""
+})
